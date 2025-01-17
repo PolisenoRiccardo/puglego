39ea011340d88674cd065f7688a1224b48b7ae7d
Poliseno Riccardo <poliseno.riccardo@iisgalvanimi.edu.it>
2025-01-18 00:38:19 +0100

    finito

--- app.js
+++ app.js
@@ -3,13 +3,29 @@ const lego = require('./lego.json'); //Copia il file people.json dentro la varia
 
 const app = express();
 
-app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
     res.render('index', {
     title: 'Homepage',
     lego: lego.set //Passa il vettore profiles alla pagina index.pug
     });
+});
+
+app.get('/set/:set_name', (req, res) => {
+    const setName = req.params.set_name;
+    const set = lego.set.find(s => s.name === setName);
+
+    if (set) {
+        res.render('set', {
+            title: set.name,
+            set: set
+        });
+    } else {
+        res.status(404).send('Set not found');
+    }
 });
 
 app.listen(3000, function () {
